9966e2cf6cdfeaf7503f97d1cc04aaef18c9eeb1
mercymugambi <mercymugambi2016@gmail.com>
2023-03-17 22:02:53 +0300

    Fix speakers section dynamically

--- js/app.js
+++ js/app.js
@@ -11,3 +11,85 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
   }));
+
+//objects 
+const featuredSpeakers = document.getElementById("featuredSpeakers");
+const moreSpeakers = document.getElementById("moreSpeakers");
+
+const allSpeakers = [
+  {
+    image: './assets/images/speaker_01 1.png',
+    speakerName: 'Yochai Benkler',
+    profession: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    carreer: 'Benkler studies commons-based peer production',
+  },
+
+  {
+    image: './assets/images/speaker_01 1.png',
+    speakerName: 'osugo Benkler',
+    profession: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    carreer: 'Benkler studies commons-based peer production',
+  },
+
+  {
+    image: './assets/images/speaker_01 1.png',
+    speakerName: 'Joes Benkler',
+    profession: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    carreer: 'Benkler studies commons-based peer production, and published his seminal book'
+  },
+
+  {
+    image: './assets/images/speaker_01 1.png',
+    speakerName: 'Kimbarly Benkler',
+    profession: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    carreer: 'Benkler studies commons-based peer production, and published his seminal book',
+  },
+
+  {
+    image: "./assets/images/speaker_01 1.png",
+    speakerName: "Grace Benkler",
+    profession: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    carreer: "Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006",
+  },
+
+  {
+    image: "./assets/images/speaker_01 1.png",
+    speakerName: "Truphena Benkler",
+    profession: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    carreer: "Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006",
+  },
+];
+
+window.addEventListener('load', (() => {
+  function addfeatured(){
+  let addfeatureSpeakers = '';
+  allSpeakers.forEach((speaker, index) =>{
+    if ((window.screen.availWidth < 768) && index <=1){
+      addfeatureSpeakers += `<div class="speakers-card">
+      <img src="${speaker.image}" alt="image1"/>
+      <div class="speakers-intro">
+          <h2 class="speakers-name">${speaker.speakerName}</h2>
+          <p class="speakers-proffession">${speaker.profession}</p>
+          <p class="speakers-career">${speaker.carreer}</p>
+      </div>
+  </div>`;
+    }else if ((window.screen.availWidth >= 768)) {
+      
+      addfeatureSpeakers += `<div class="speakers-card">
+      <img src="${speaker.image}" alt="image1"/>
+      <div class="speakers-intro">
+          <h2 class="speakers-name">${speaker.speakerName}</h2>
+          <p class="speakers-proffession">${speaker.profession}</p>
+          <p class="speakers-career">${speaker.carreer}</p>
+      </div>
+  </div>`;
+    }
+  })
+  featuredSpeakers.innerHTML = addfeatureSpeakers;
+  }
+  addfeatured();
+
+  window.onresize = addfeatured;
+  
+}));
+
